19228aee074e0baec125e2d5762cb33aa2e3cf9f
ahcl <ahcl4545@qq.com>
2017-02-23 15:41:52 +0800

    mine

---
+++ pages/mine/mine.js
@@ -0,0 +1,43 @@
+//index.js
+import keys from '../../config/keys.js'
+//获取应用实例
+var app = getApp()
+Page({
+    data: {
+    },
+    //事件
+    setting:function(){
+        console.log("setting");
+    },
+    sleepZoneTap:function(){
+        console.log("sleepZoneTap");
+    },
+    braceletTap:function(){
+         console.log("braceletTap");
+    },
+    otherTap:function(){
+        console.log("otherTap");
+    },
+    deleteTap:function(){
+        console.log("deleteTap");
+    },
+    ownTap:function(){
+        console.log("ownTap");
+    },
+    useHelpTap:function(){
+        console.log("useHelpTap");
+    },
+    downLoadTap:function(){
+        console.log("downLoadTap");
+    },
+    onLoad: function (options) {
+        let that = this;
+        console.log(" onLoad mine");
+        app.getUserInfo(function (userInfo) {
+            //更新数据
+            that.setData({
+                userInfo: userInfo
+            })
+        })
+    }
+})
